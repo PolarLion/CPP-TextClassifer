f667f38091e8d9138a138566d0a63ad35b224598
RainAndSnow <545135082@qq.com>
2014-12-30 12:21:13 +0800

    alubuntu

--- Classifier.h
+++ Classifier.h
@@ -23,10 +23,10 @@ public:
   Classifier() : class_num (0), features_num(0), training_size(0), is_free(true) {}
   virtual ~Classifier(){}
   // virtual void init_model(const char* config_file) = 0;
-	virtual void train_on_file(const char* training_file) = 0;
-	virtual void save_model(const char* model_file) = 0;
-  virtual void load_model(const char* model_file) = 0;
-	virtual void predicted_category(const double* features, int& res) = 0;
-  virtual void free_model() = 0;
-  virtual void show_model() {}
+	virtual bool train_on_file(const char* training_file) = 0;
+	virtual bool save_model(const char* model_file) = 0;
+  virtual bool load_model(const char* model_file) = 0;
+	virtual bool predicted_category(const double* features, int& res) = 0;
+  virtual bool free_model() = 0;
+  virtual bool show_model() {}
 };
