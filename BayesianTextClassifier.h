717cab806d473a7f675542f503718d0be1b44394
RainAndSnow <545135082@qq.com>
2014-12-30 12:25:35 +0800

    alubuntu

--- BayesianTextClassifier.h
+++ BayesianTextClassifier.h
@@ -14,18 +14,18 @@ class BayesianTextClassifier:public Classifier
 private:
   Item *bayesiantable;
   
-  void show_bayesiantable();
+  bool show_bayesiantable();
   bool init_bayesiantable(int f, int c);
 public:
   BayesianTextClassifier();
   ~BayesianTextClassifier();
   // void init_model(const char* config_file);
-  void train_on_file(const char* training_file);
-  void save_model(const char* model_file);
-  void load_model(const char* model_file);
-  void predicted_category(const double* features, int& res);
-  void free_model();
-  void show_model();
+  bool train_on_file(const char* training_file);
+  bool save_model(const char* model_file);
+  bool load_model(const char* model_file);
+  bool predicted_category(const double* features, int& res);
+  bool free_model();
+  bool show_model();
 };
 
 }
--- LogitRegressionClassifier.h
+++ LogitRegressionClassifier.h
@@ -15,12 +15,12 @@ public:
   LogitRegressionClassifier ();
   LogitRegressionClassifier (const char* config_file);
   ~LogitRegressionClassifier ();
-  void init_model (const char* config_file);
-  void train_on_file (const char* training_file);
-  void save_model (const char* model_file);
-  void load_model (const char* model_file);
-  void predicted_category (const double* features, int& res);
-  void free_model ();
+  bool init_model (const char* config_file);
+  bool train_on_file (const char* training_file);
+  bool save_model (const char* model_file);
+  bool load_model (const char* model_file);
+  bool predicted_category (const double* features, int& res);
+  bool free_model ();
 };
 
 }
